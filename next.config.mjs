79fc618863fe179204a33f86378b2294ed9261c7
Ryan MacIntosh <rmacintosh@gmail.com>
2025-10-22 20:46:39 +0000

    refactor: Remove custom image loader and rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    loader: 'custom',
-    loaderFile: './loader.js',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/_fah/image/process',
-        destination: 'https://apphosting.us-central1.run.app/backends/tronicslab-fpga-firebase/image/process',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
