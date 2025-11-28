89cee2bde663ae3787064148936392c420d0643f
Ryan MacIntosh <rmacintosh@gmail.com>
2025-11-28 16:04:02 +0000

    Runtime Error
    Failed to parse private key: Error: Invalid PEM formatte

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -6,6 +6,12 @@ let app: App;
 if (!getApps().length) {
   if (process.env.SERVICE_ACCOUNT_KEY_JSON) {
     const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY_JSON);
+    
+    // Replace escaped newlines in the private key
+    if (serviceAccount.private_key) {
+        serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
+
     app = initializeApp({
       credential: admin.credential.cert(serviceAccount),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
